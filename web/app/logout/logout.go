a6ae00275c0684dd31473d44569953a6aae9af9c
Tyler Gabb <tylerjgabb@gmail.com>
2024-04-22 07:51:44 -0700

    update to use oidc logout

--- web/app/logout/logout.go
+++ web/app/logout/logout.go
@@ -11,25 +11,26 @@ import (
 
 // Handler for our logout.
 func Handler(ctx *gin.Context) {
-	logoutUrl, err := url.Parse("https://" + os.Getenv("AUTH0_DOMAIN") + "/v2/logout")
+	logoutUrl, err := url.Parse("https://" + os.Getenv("AUTH0_DOMAIN") + "/oidc/logout")
 	if err != nil {
 		ctx.String(http.StatusInternalServerError, err.Error())
 		return
 	}
-
+	
 	scheme := "http"
 	if ctx.Request.TLS != nil {
 		scheme = "https"
 	}
-
+	
+	// how is this removing the session cookie?
 	returnTo, err := url.Parse(scheme + "://" + ctx.Request.Host)
 	if err != nil {
 		ctx.String(http.StatusInternalServerError, err.Error())
 		return
 	}
-
-	// https://auth0.com/docs/authenticate/login/logout/log-users-out-of-auth0#oidc-logout-endpoint-parameters
+	
 	parameters := url.Values{}
+	// https://auth0.com/docs/authenticate/login/logout/log-users-out-of-auth0#oidc-logout-endpoint-parameters
 	parameters.Add("post_logout_redirect_uri", returnTo.String())
 	parameters.Add("client_id", os.Getenv("AUTH0_CLIENT_ID"))
 	logoutUrl.RawQuery = parameters.Encode()
